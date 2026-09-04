791f8733dfb343c63432724497297678fb6f6c54
Abhishek Dudhani <abhishek.dudhani@hotmail.com>
2026-09-05 00:46:26 +0530

    Array questions

---
+++ src/ArrayQuestions/RemoveDuplicateElementInArray_08.java
@@ -0,0 +1,74 @@
+package ArrayQuestions;
+
+import java.util.Arrays;
+
+public class RemoveDuplicateElementInArray_08 {
+
+    /*
+     * Input = {20, 25, 7, 45, 18, 99, 99, 80, 70}
+     *
+     * Expected = 99
+     *
+     * Problem:
+     * Find the duplicate element of an array.
+     */
+    public static void main(String[] args) {
+        int[] arr = {20, 25, 9, 45, 18, 994, 9, 80, 70};
+        int isDupe = checkDuplicateElement(arr);
+        int[] result = removeDuplicateElement(arr);
+        int isDupeAfter = checkDuplicateElement(result);
+
+        System.out.println(Arrays.toString(result));
+        if (isDupe != 0) {
+            System.out.println("Duplicate present : " + isDupe);
+        } else {
+            System.out.println("No Duplicate present");
+        }
+        if (isDupeAfter != 0) {
+            System.out.println("Duplicate present : " + isDupeAfter);
+        } else {
+            System.out.println("No Duplicate present");
+        }
+
+    }
+
+    public static int checkDuplicateElement(int[] arr) {
+
+        for (int i = 0; i < arr.length - 1; i++) {
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[i] == arr[j]) {
+                    return arr[i];
+                }
+            }
+        }
+        return 0;
+    }
+
+    public static int[] removeDuplicateElement(int[] arr) {
+
+        int[] result = new int[arr.length];
+        int index = 0;
+
+        for (int i = 0; i < arr.length; i++) {
+
+            boolean alreadyPresent = false;
+
+            for (int j = 0; j < index; j++) {
+                if (arr[i] == result[j]) {
+                    alreadyPresent = true;
+                    break;
+                }
+            }
+
+            if (!alreadyPresent) {
+                result[index] = arr[i];
+                index++;
+            }
+        }
+
+        return Arrays.copyOf(result, index);
+    }
+}
+
+
+
